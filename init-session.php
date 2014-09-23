aae32bcaf99362bbbfaf6fc7bab16be55c5efdf6
Uzo Olisemeka <uzo@designbymobius.ca>
2014-09-23 23:17:39 +0100

    cookieless sessions

--- init-session.php
+++ init-session.php
@@ -10,8 +10,11 @@
 		$user_signature = $_POST['signature'];		
 		$server_signature = getenv('PRICINGAPP_SIGNATURE');
 
+	// cookieless sessions 
+		ini_set('session.use_cookies',0);
+
 	// memcached server connection config
-		ini_set('session.save_handler=memcached');
+		ini_set('session.save_handler','memcached');
 		ini_set('session.save_path', 'PERSISTENT=myapp_session ' . $_ENV['MEMCACHEDCLOUD_SERVERS']);
 		ini_set('memcached.sess_binary', 1);
 		ini_set('memcached.sess_sasl_username', $_ENV['MEMCACHEDCLOUD_USERNAME']);
