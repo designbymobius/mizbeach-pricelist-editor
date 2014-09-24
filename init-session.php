f9b8514e6d1b8a9508e02e8618aa245e2133bfd0
Uzo Olisemeka <uzo@designbymobius.ca>
2014-09-24 09:49:26 +0100

    fishing in the dark here

--- init-session.php
+++ init-session.php
@@ -4,7 +4,7 @@
 		header( 'Access-Control-Allow-Origin: *' );
 
 	// load JWT class
-		require('vendor/firebase/php-jwt/Authentication/JWT.php'); 
+		require('Vendor/autoloader.php'); 
 
 	// no signature no token
 		if ( !isset($_POST['signature']) ){ exit('cool.story.bro'); }
